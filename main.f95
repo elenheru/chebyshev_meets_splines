ae16c7e9d65f4946186aa0b89a1394f5d053e93d
Aleksei Gusevy <elenheru@ya.ru>
2018-09-23 23:56:02 +0300

    initial

---
+++ main.f95
@@ -0,0 +1,20 @@
+!MS$DECLARE
+! !; is staring when pseudocoding ; !@ is starting when clarification is needed
+! when exclamation sign is on top-left that is debugging command
+! uppercase of EXIT RETURN - for debugging
+! lowercase of exit return - for proper work
+! to create a directory !: call execute_command_line('mkdir directoryname',WAIT=.TRUE.)
+
+program    chebyshev_meets_splines
+    use what_you_need_mod
+    integer i,j
+    real ancil1
+
+!;  zadatq polinom dlqa priblizxenija
+!;  zadatq splajnovyje znacxenija 0 i 1 proizvodnyh
+!;  na koncah scxitajem sxto splajn sovpadajet s chebysxevskim polinomom
+!;  rasscxitatq proizvodnyje osxibki po kazxdomy koefficientu
+!;  spustitsia vdolq gradienta
+
+endprogram chebyshev_meets_splines
+
